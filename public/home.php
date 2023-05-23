001386f8d84fe08c65e77bb93c1f0596c03efccb
viovaa <olivaresvaldesvicente@gmail.com>
2023-05-23 13:07:33 -0400

    Casos de uso

--- public/home.php
+++ public/home.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu</title>
+    <title>sistema de control de vehículos</title>
+    <link rel="shortcut icon" href="./img/favicon-scv.png" type="image/x-icon">
 </head>
 <body>
-    <h1>Menu</h1>
+
 </body>
 </html>
--- public/src/iniciarsecion.php
+++ public/src/iniciarsecion.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>registrar vehículo particular</title>
-    <link rel="shortcut icon" href="../../public/img/favicon-scv.png" type="image/x-icon">
+    <title>iniciar seción</title>
+    <link rel="shortcut icon" href="../img/favicon-scv.png" type="image/x-icon">
 </head>
 <body>
     
