44543db641b58afddcaff5c554bfca7d75ea5865
Jorge Paredes Vargas <134024470+JorgeParedesV@users.noreply.github.com>
2023-05-30 21:26:18 -0400

    Update code
    redirecciones

--- view/agenteAduanal/agenteaduanal-home-listartramitesvehiculodecarga.php
+++ view/agenteAduanal/agenteaduanal-home-listartramitesvehiculodecarga.php
@@ -5,23 +5,48 @@
   <title>Administrador</title>
   <link rel="stylesheet" href="../../public/css/style.css">
   <link rel="shortcut icon" href="../../public/img/favicon-scv.png" type="image/x-icon">
+
+  
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    
   
 </head>
 <body>
     <h1>Bienvenido</h1>
     <div>
-    <p>Pulse para acceder a la lista de documentos</p>
+    <p>Seleccione Acciones...</p>
     
     <div>
-        <button class="button" onclick="redirectToDocumentos()">Lista de Documentos</button>
+        
     </div>
-  
-    <form action="../../public/src/cerrarsesion.php" method="POST" class="btn btn-secondary btn-regresar">
-        <input type="submit" value="Cerrar Sesión">
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Pulse para acceder a Lista de Documentos</h4>
+            <p class="card-text"><button type="button" class="btn btn-dark" onclick="redirectToDocumentos()">Lista de Documentos</button></p>
+            <h4 class="card-title">Pulse para acceder a Seleccionar Tramite</h4>
+            <p class="card-text"><button type="button" class="btn btn-dark" onclick="redirectToSeleccionarTramite()">Seleccionar Tramite</button></p>
+            <h4 class="card-title">Pulse para acceder a Seleccionar Vehiculo</h4>
+            <p class="card-text"><button type="button" class="btn btn-dark" onclick="redirectToSeleccionarVehiculoDeCarga()">Seleccionar Vehiculo</button></p>
+            
+            <form action="../../public/src/cerrarsesion.php" method="POST" class="btn btn-secondary btn-regresar">
+                <input type="submit" value="Cerrar Sesión">
+        </div>
+    </div>
+
 
     <script>
     function redirectToDocumentos() {
         window.location.href = "agenteaduanal-documentos.php";
+    }
+
+    function redirectToSeleccionarTramite() {
+        window.location.href = "agenteaduanal-seleccionartramite.php";
+    }
+
+    function redirectToSeleccionarVehiculoDeCarga() {
+        window.location.href = "agenteaduanal-seleccionarvehiculodecarga.php";
     }
        
     </script>
